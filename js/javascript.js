bd1db80234824f53b5dcd2bfc28cd00d94d0a4f7
zhenailuoye1 <122676065@qq.com>
2017-06-13 18:23:04 +0800

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,29 @@
+$(document).ready(function (){
+    //todo面板
+    $("#add-todo").click(function () {
+        var title = $("#todo-title").val();
+        var content = $("#todo-content").val();
+        var todo = ''+
+        '<div class="panel panel-default">'+
+            '<div class="panel-heading">'+
+                '<h3 class="panel-title">'+
+                    title+
+                '</h3>'+
+            '</div>'+
+            '<div class="panel-body">'+ 
+                content+
+            '</div>'+
+        '</div>'
+        if (!title) {
+            alert("标题不能为空!");
+        } else if (!content) {   
+            alert("内容不能为空！");
+        } else {
+            $("#undo").prepend(todo);
+            $("#todo-title").val('');
+            $("#todo-content").val('');
+        }
+    })
+
+    $("#add-todo").css("height",$("#todo-content").css("height"))
+}) 
