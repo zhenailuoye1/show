6db27ed63db6fbe9bc9d82c2f413bb13463ec5c5
zhenailuoye1 <122676065@qq.com>
2017-06-14 10:03:34 +0800

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,50 @@
+require.config({
+　　　　paths: {
+　　　　　　jquery: "jquery.min",
+　　　　　　bootstrap: "bootstrap.min"
+　　　　},
+		shim: {
+			bootstrap: {
+				deps: ["jquery"]
+			}
+		}
+　　});
+
+
+require(['jquery','bootstrap'],function($){
+	
+	$(document).ready(function (){
+		//todo面板
+		$("#add-todo").click(function () {
+			var title = $("#todo-title").val();
+			var content = $("#todo-content").val();
+			var todo = ''+
+			'<div class="panel panel-default">'+
+				'<div class="panel-heading">'+
+					'<h3 class="panel-title">'+
+						title+
+					'</h3>'+
+				'</div>'+
+				'<div class="panel-body">'+ 
+					content+
+				'</div>'+
+			'</div>'
+			if (!title) {
+				alert("标题不能为空!");
+			} else if (!content) {   
+				alert("内容不能为空！");
+			} else {
+				$("#undo").prepend(todo);
+				$("#todo-title").val('');
+				$("#todo-content").val('');
+			}
+		})
+
+		$("#add-todo").css("height",$("#todo-content").css("height"))
+		
+		$(".come_soon").click(function () {
+			alert("本板块尚未开放");
+		})
+		
+	}) 
+})
